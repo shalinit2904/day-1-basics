26229ceaae67c8f0ebf37581e0ad9ecc0ece4066
shalinit2904 <shalinit200429@gmail.com>
2025-05-12 14:28:26 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum=0;
+    printf("enter a numbers :");
+    scanf("%d",&a);
+    printf("enter a number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("the sum of two num is%d",sum);
+}
